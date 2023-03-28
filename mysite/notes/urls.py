52fa7852f92fbd0f4222b640dfcad024f11ba726
imac 6 <imac6@imacs-iMac.local>
2023-03-28 14:16:25 +0300

    Initial commit

--- mysite/notes/urls.py
+++ mysite/notes/urls.py
@@ -10,10 +10,4 @@ router.register(r'note', NoteViewSet)
 
 urlpatterns = [
     path('api/v1/', include(router.urls)),
-
-    # path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    # path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    # path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc')
-    # path('v1/notelist/', NoteViewSet.as_view({'get': 'list'})),
-    # path('v1/notelist/<int:pk>/', NoteViewSet.as_view({'get': 'update'}))
 ]
